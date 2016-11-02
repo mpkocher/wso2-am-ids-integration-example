b8a33d1746024ad8d75e8ddce4b4dddcc8942e00
mpkocher <michael.kocher@me.com>
2016-11-01 19:19:36 -0700

    add identity.xml and update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,2 +1,6 @@
 cp wso2am-2.0.0/repository/conf/datasources/master-datasources.xml ../wso2am-2.0.0/repository/conf/datasources/master-datasources.xml
 cp wso2is-5.2.0/repository/conf/datasources/master-datasources.xml ../wso2is-5.2.0/repository/conf/datasources/master-datasources.xml
+
+# update the original indentity.xml in APIM to IDS
+cp wso2am-2.0.0/repository/conf/identity/identity.xml ../wso2am-2.0.0/repository/conf/identity/identity.xml
+cp wso2am-2.0.0/repository/conf/identity/identity.xml ../wso2is-5.2.0/repository/conf/identity/identity.xml
