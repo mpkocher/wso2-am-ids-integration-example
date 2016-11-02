5414c0a7f29495be1297622473f1fd7451175c89
mpkocher <michael.kocher@me.com>
2016-11-01 19:22:42 -0700

    Add api-manager.xml

--- updater.sh
+++ updater.sh
@@ -4,3 +4,9 @@ cp wso2is-5.2.0/repository/conf/datasources/master-datasources.xml ../wso2is-5.2
 # update the original indentity.xml in APIM to IDS
 cp wso2am-2.0.0/repository/conf/identity/identity.xml ../wso2am-2.0.0/repository/conf/identity/identity.xml
 cp wso2am-2.0.0/repository/conf/identity/identity.xml ../wso2is-5.2.0/repository/conf/identity/identity.xml
+
+# update api-manager.xml from AM to IDS
+cp wso2am-2.0.0/repository/conf/api-manager.xml ../wso2am-2.0.0/repository/conf/api-manager.xml
+cp wso2am-2.0.0/repository/conf/api-manager.xml ../wso2is-5.2.0/repository/conf/api-manager.xml
+
+# Even
