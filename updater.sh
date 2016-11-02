b1f9d5cc0fce97d0672d38ac1696efe2bac3d49b
mpkocher <michael.kocher@me.com>
2016-11-01 19:07:45 -0700

    add updater

---
+++ updater.sh
@@ -0,0 +1,2 @@
+cp wso2am-2.0.0/repository/conf/datasources/master-datasources.xml ../wso2am-2.0.0/repository/conf/datasources/master-datasources.xml
+cp wso2is-5.2.0/repository/conf/datasources/master-datasources.xml ../wso2is-5.2.0/repository/conf/datasources/master-datasources.xml
